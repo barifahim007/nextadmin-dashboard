b9fa1366422cd537b79be8df816425bf7ada5d75
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-11-09 15:27:15 +0600

    side dashboard nav

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import styles from "../sidebar/sidebar.module.css";
 import menuItems from "./menuItem";
 import MenuLink from "./menuLink/menuLlink";
+import Image from "next/image";
+import noavatart from "./../../../../public/noavatar.png";
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-      <ul>
+      <div className={styles.userContainer}>
+        <Image
+          className={styles.userProfile}
+          src={noavatart}
+          width={50}
+          height={50}
+          alt="noavatar"
+        />
+        <div className={styles.userDetails}>
+          <span className={styles.userTitle}>Ehsanul Bari Fahim</span>
+          <span className={styles.userRole}>Adminstrator</span>
+        </div>
+      </div>
+
+      <ul className={styles.list}>
         {menuItems.map((category) => (
           <li key={category.title}>
-            <span>{category.title}</span>
+            <span className={styles.category}>{category.title}</span>
             {category.items.map((item) => (
               <MenuLink item={item} key={item.title} />
             ))}
