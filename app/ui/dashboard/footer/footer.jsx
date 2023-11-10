17eaad386a775f9f40f2803c622634490f68091e
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-11-10 22:51:50 +0600

    sidebar and footer

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./footer.module.css";
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>EHSANUL BARI FAHIM</div>
+      <div className={styles.text}> @ 2023 all right reseverd</div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import styles from "./rightbar.module.css";
+import Image from "next/image";
+import { MdArrowCircleRight, MdPlayCircle } from "react-icons/md";
 
 const Rightbar = () => {
-  return <div>Rightbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContent}>
+          <Image
+            src="/astronaut.png"
+            alt="astronaut"
+            fill
+            className={styles.bgimg}
+          />
+        </div>
+        <div className={styles.text}>
+          <h1 className={styles.textTitle}>available now</h1>
+          <h2 className={styles.textSecTitle}>
+            How to use new version of admin dashboard.
+          </h2>
+          <span className={styles.textspan}> takes 4 minutes to learn</span>
+          <p className={styles.textpara}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore,
+            beatae.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircle />
+            watch
+          </button>
+        </div>
+      </div>
+      {/* second div */}
+      <div className={styles.item}>
+        <div className={styles.text}>
+          <h1 className={styles.textTitle}>Comming soon</h1>
+          <h2 className={styles.textSecTitle}>
+            new server actions are available partial pre-rendering is comming up
+          </h2>
+          <span className={styles.textspan}> boost your productivity</span>
+          <p className={styles.textpara}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore,
+            beatae.
+          </p>
+          <button className={styles.button}>
+            <MdArrowCircleRight />
+            learn more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
