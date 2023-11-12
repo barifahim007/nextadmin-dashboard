1bab32495b3225da983d78b1b0fdb45edf9e7d47
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-11-12 16:25:21 +0600

    add new product and user

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "@/app/ui/dashboard/products/addProducts/addproducts.module.css";
+
+const AddProducts = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" name="title" placeholder="title" required />
+        <select name="category" id="category">
+          <option value="generel">Choose a option</option>
+          <option value="kitchen">kitchen</option>
+          <option value="phone">phone</option>
+          <option value="computer">computer</option>
+        </select>
+        <input type="number" name="price" placeholder="price" required />
+        <input type="number" name="stock" placeholder="stock" />
+        <input type="text" name="color" placeholder="size" />
+        <input type="text" name="size" placeholder="size" />
+        <textarea
+          name="desc"
+          id="desc"
+          rows="16"
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProducts;
---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "@/app/ui/dashboard/users/addUser/addUser.module.css";
+const AddProducts = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" name="username" placeholder="username" required />
+        <input type="email" name="email" placeholder="email" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+        />
+        <input type="phone" name="Phone" placeholder="Phone" />
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>
+            Is Admin?
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={true} selected>
+            Is Active?
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+
+        <textarea
+          name="address"
+          id="address"
+          rows="16"
+          placeholder="address"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProducts;
